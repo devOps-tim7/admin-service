5f5c25680167735a7c2c7d15d1f31f40df8d71ee
Marko131 <markostanic222@gmail.com>
2021-06-20 10:25:10 +0200

    Add metrics for prometheus

--- server.ts
+++ server.ts
@@ -6,11 +6,15 @@ import PostRoutes from './src/routes/Post';
 import UserRoutes from './src/routes/User';
 import ReportRoutes from './src/routes/Report';
 import AgentRequestRoutes from './src/routes/AgentRequest';
+import metricsMiddleware from './src/middleware/MetricsMiddleware';
+import MetricsRoutes from './src/routes/Metrics';
 
 export const createServer = () => {
   const app = express();
   app.use(express.json({ limit: '8mb' }));
   app.use(cors());
+  app.use(metricsMiddleware);
+  app.use('/metrics', MetricsRoutes);
   app.use('/api/admin/posts', PostRoutes);
   app.use('/api/admin/users', UserRoutes);
   app.use('/api/admin/reports', ReportRoutes);
