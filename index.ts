6048b878e3493678f4c3275ee6b0162bb62fd87f
Marko131 <markostanic222@gmail.com>
2021-06-22 20:27:07 +0200

    Calculate traffic in megabytes and add metrics (#10)

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import { createConnection } from 'typeorm';
 import { createServer } from './server';
 import connection from './src/helpers/Connection';
+import RequestStatsMiddleware from './src/middleware/RequestStatsMiddleware';
 
 createConnection()
   .then(() => connection.clear())
@@ -11,7 +12,8 @@ createConnection()
 
     const port = process.env.PORT;
 
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       console.log(`Server has started at http://localhost:${port}`);
     });
+    RequestStatsMiddleware(server)    
   });
